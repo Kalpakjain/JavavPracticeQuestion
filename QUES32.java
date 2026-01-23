f9100d74612f26b1663d2e2c5eb0f275a009e3a2
Kalpak jain <Kalpakjain158@gmail.com>
2026-01-23 17:44:32 +0530

    Question 32 solved

---
+++ QUES32.java
@@ -0,0 +1,17 @@
+//32. Take two numbers and check if both are positive and their sum is less than 100.
+import java.util.*;
+public class QUES32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        if(num1 > 0 && num2 > 0 && (num1 + num2 < 100)){
+            System.out.println("Both numbers are positive and their sum is less than 100.");
+        }else{
+            System.out.println("Either one or both numbers are not positive or their sum is not less than 100.");
+        }
+        sc.close();
+    }
+}
