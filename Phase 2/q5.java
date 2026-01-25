fc3cc6b5225c14ea0ad66887016e00be07f33bf6
Kalpak jain <Kalpakjain158@gmail.com>
2026-01-25 22:55:32 +0530

    Question 5 solved

---
+++ Phase 2/q5.java
@@ -0,0 +1,9 @@
+//5. Print the table of a given number (n × 1 to n × 10).
+public class q5 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=10 ; i++){
+            System.out.println(n + "x" + i + "=" + n*i);
+        }
+    }
+}
