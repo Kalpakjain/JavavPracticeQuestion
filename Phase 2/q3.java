62d556a43aad112a385b4d5718c0c8349a5c18b5
Kalpak jain <Kalpakjain158@gmail.com>
2026-01-25 22:53:59 +0530

    Question 3 solved

---
+++ Phase 2/q3.java
@@ -0,0 +1,10 @@
+//3. Print all odd numbers between 1 and 100.
+public class q3 {
+    public static void main(String[] args) {
+        for(int i = 0; i<=100; i++){
+            if(i%2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
