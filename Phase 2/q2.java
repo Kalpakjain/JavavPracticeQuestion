09601a2463b669c8dce9c21ee929926b0f96fb06
Kalpak jain <Kalpakjain158@gmail.com>
2026-01-25 22:53:38 +0530

    Question 2 solved

---
+++ Phase 2/q2.java
@@ -0,0 +1,11 @@
+//2. Print all even numbers between 1 and 100.
+public class q2 {
+    public static void main(String[] args) {
+        for(int i=0; i<=100; i+=2 /* i++ */){
+            System.out.println(i);
+            /* if(i%2 ==0){
+            System.out.println("i")} */
+        }
+    }
+    
+}
