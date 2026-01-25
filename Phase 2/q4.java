309e2ce34867200a70e205b590dcbd7a3e608871
Kalpak jain <Kalpakjain158@gmail.com>
2026-01-25 22:55:25 +0530

    Question 4 solved

---
+++ Phase 2/q4.java
@@ -0,0 +1,8 @@
+//4. Print numbers from 10 down to 1.
+public class q4 {
+    public static void main(String[] args) {
+        for(int i =10; i>0; i-- ){
+            System.out.println(i);
+        }
+    }
+}
