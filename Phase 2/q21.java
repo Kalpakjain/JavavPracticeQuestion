2432327e789417e93427588cafd5351f68f829db
Kalpak jain <Kalpakjain158@gmail.com>
2026-01-29 19:31:55 +0530

    Question 21 solved

---
+++ Phase 2/q21.java
@@ -0,0 +1,9 @@
+//21. Print the squares of numbers from 1 to n.
+public class q21 {
+    public static void main(String[] args) {
+        int n =10;
+        for(int i=1; i<=n; i++){
+            System.out.println("Square of " + i + " is = " + i*i);
+        }
+    }
+}
