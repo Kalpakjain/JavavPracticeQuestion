0dc4250fe74e705676bd0eea6b53d6a2b2c745f8
Kalpak jain <Kalpakjain158@gmail.com>
2026-01-29 19:32:05 +0530

    Question 22 solved

---
+++ Phase 2/q22.java
@@ -0,0 +1,9 @@
+//22. Print cubes of numbers from 1 to n.
+public class q22 {
+     public static void main(String[] args) {
+        int n = 10;
+        for(int i=1; i<=n; i++){
+            System.out.println("Cube of " + i + " is = " + i*i*i);
+        }
+    }
+}
